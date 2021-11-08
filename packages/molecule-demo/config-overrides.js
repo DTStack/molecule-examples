18d8d9be06720d8ef9bc630dceb3e6016d226ffe
xiaowei <wewoor@gmail.com>
2021-11-08 17:19:14 +0800

    build: integrate with MonacoWebpackPlugin

--- packages/molecule-demo/config-overrides.js
+++ packages/molecule-demo/config-overrides.js
@@ -1,5 +1,12 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
 /* config-overrides.js */
 module.exports = function override(config, env) {
     //do stuff with the webpack config...
+    config.plugins = [
+        ...config.plugins,
+        new MonacoWebpackPlugin([ 'javascript','typescript','css','html','json' ])
+    ]
+
     return config;
 }
